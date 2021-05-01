a7a830a68fc8b7d210db95cf5b94f5ad7b3923cc
tonylch <tony.lachaise@ynov.com>
2021-05-01 15:32:26 +0200

    init fastify

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const path = require('path');
 const fastify = require('fastify')({ logger: true });
-const mongoose = require('mongoose');
-const Discord = require('discord.js');
 
-// Get rid of the warnings
-const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/'
+});
 
-mongoose.connect('mongodb+srv://user_test:password_test@b3-nodejs.sbjxa.mongodb.net/jdr-tool', dbOptions, (err) => {
-    if (err)
+fastify.get('/', function (req, reply) {
+    return reply.sendFile('index.html');
+});
+
+const PORT = 3000 || process.env.PORT;
+const start = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (err) {
+        fastify.log.error(err);
         process.exit(1);
-
-    console.log('Connected!');
-})
+    }
+};
+start();
