df5c9adba6ec1dc5d4ef8786023b904807927fc3
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-05-03 15:36:50 +0200

    adding route for the role

--- server.js
+++ server.js
@@ -7,8 +7,44 @@ fastify.register(require('fastify-static'), {
 });
 
 fastify.get('/', function (req, reply) {
+    //console.log(req.query.role);
+    if (req.query.role == 'player'){
+        return reply.redirect('/player');
+    }
+
+    if (req.query.role == 'gm'){
+        return reply.redirect('/gm')
+    }
+
     return reply.sendFile('index.html');
 });
+
+fastify.get('/player', function (req, reply) {
+    return reply.sendFile('player.html');
+});
+
+fastify.get('/gm', function (req, reply) {
+    return reply.sendFile('gm.html');
+});
+
+/*const opts = {
+
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+fastify.get('/test', opts, function (request, reply) {
+
+    //console.log(request.body);
+    reply.send({ hello: 'world'});
+});*/
 
 const PORT = 3000 || process.env.PORT;
 const start = async () => {
