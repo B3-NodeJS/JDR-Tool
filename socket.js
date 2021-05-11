02288a2d26428d292fb8826bc2013e356c7e0e90
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-05-11 23:38:24 +0200

    Ajout style + check dans les sockets
    Check si le message envoyé commence par "!"
    Ajout du style de la page gameMaster et Player

--- socket.js
+++ socket.js
@@ -9,20 +9,37 @@ async function socket(fastify) {
     fastify.ready(err => {
 
         if (err) {
-
             console.log(err)
         }
 
         fastify.io.on('connect', (socket) => {
 
             console.log('Connected : ' + socket.rooms);
-            socket.on('Chat message', (msg) => {
 
+            socket.on('Chat message', (msg) => {
                 console.log('message: ' + msg);
                 fastify.io.emit('Chat message', msg);
-            });
-            socket.on('disconnect', () => {
 
+                if (msg.startsWith('!')) {
+                    let str = msg.substr(1).split(' ');
+
+                    switch(str[0]) {
+                        case "create":
+                            fastify.io.emit('Chat message', 'CEST LA COMMANDE DE CREATION');
+                            break;
+                        case "update":
+                            fastify.io.emit('Chat message', 'CEST LA COMMANDE DE MODIFICATION');
+                            break;
+                        case "delete":
+                            fastify.io.emit('Chat message', 'CEST LA COMMANDE DE SUPPRESSION');
+                            break;
+                        default:
+                            fastify.io.emit('Chat message', 'Cette commande est inconnue');
+                    }
+                }
+            });
+
+            socket.on('disconnect', () => {
                 console.log('Disconnect');
             });
         });
